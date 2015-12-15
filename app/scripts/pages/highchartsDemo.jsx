7f34b0373db1b555d97bb1f0756655a62d66f58f
Pradeep kumar <pradeep.ksm@flipkart.com>
2015-12-15 13:38:10 +0530

    try

--- app/scripts/pages/highchartsDemo.jsx
+++ app/scripts/pages/highchartsDemo.jsx
@@ -21,7 +21,7 @@ var config = {
             name: 'Jane',
             data: [1, 0, 4, 5]
         }, {
-            name: 'JohnPradeep',
+            name: 'poll',
             data: [5, 7, 3, 103]
         }]
 };
